fd14f6343268e4ebd6c0b365e5f59ac1381da970
sunny <sunmeh2525@gmail.com>
2018-03-09 02:18:25 +0530

    new

--- app/src/main/java/com/work/one/videoplayeranimation/VideoViewAnimationActivity.java
+++ app/src/main/java/com/work/one/videoplayeranimation/VideoViewAnimationActivity.java
@@ -1,17 +1,42 @@
 package com.work.one.videoplayeranimation;
 
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.util.DisplayMetrics;
 import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
-public class VideoViewAnimationActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class VideoViewAnimationActivity extends AppCompatActivity {
+
+    private MediaMetadataRetriever mediaMetadataRetriever;
+    private MediaController myMediaController;
+    private boolean isCompleted = false;
+    private MediaPlayer.OnCompletionListener myVideoViewCompletionListener =
+            new MediaPlayer.OnCompletionListener() {
+
+                @Override
+                public void onCompletion(MediaPlayer arg0) {
+                    isCompleted = true;
+                    Toast.makeText(VideoViewAnimationActivity.this, "End of Video",
+                            Toast.LENGTH_LONG).show();
+                }
+            };
     private VideoView vvRandomView;
-    private Button btnSmall, btnLarge;
+    private TextView tvStatus;
     private boolean isSmall = false;
+    private int height;
+    private int width;
+    private TimerTask timerTask;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,33 +46,83 @@ public class VideoViewAnimationActivity extends AppCompatActivity implements Vie
         play();
     }
 
-    private void play() {
-        MediaController mediaController = new MediaController(this);
+    private void startTimer() {
+        Timer timer;
+        handler = new Handler();
 
-        mediaController.setAnchorView(vvRandomView);
-        mediaController.setAnchorView(vvRandomView);
+        timer = new Timer();
+
+        //initialize the TimerTask's job
+        initializeTimerTask();
+
+        timer.schedule(timerTask, 0, 5000);
+    }
+
+    public void initializeTimerTask() {
+        timerTask = new TimerTask() {
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        //code to run after every 5 seconds
+                        if (isSmall) {
+                            changeVideoSize(1000, 1000);
+                            isSmall = false;
+                        } else {
+                            changeVideoSize(500, 500);
+                            isSmall = true;
+                        }
+                    }
+                });
+            }
+        };
+    }
+
+    private void changeVideoSize(int newWidth, int newHeight) {
+
+        int left = vvRandomView.getLeft();
+        int top = vvRandomView.getTop();
+        int right = left + (newWidth);
+        int bottom = top + (newHeight);
+        vvRandomView.layout(left, top, right, bottom);
+//        if(!isSmall){
+//            vvRandomView.setClipToOutline();
+//        }
+
+    }
+
+    private void play() {
+
+        String viewSource = "android.resource://" + getPackageName() + "/" + R.raw.videoplayback;
+        mediaMetadataRetriever = new MediaMetadataRetriever();
+        mediaMetadataRetriever.setDataSource(this, Uri.parse(viewSource));
+
+
+        vvRandomView.setVideoURI(Uri.parse(viewSource));
+        myMediaController = new MediaController(this);
+        vvRandomView.setMediaController(myMediaController);
+
+        /*vvRandomView.setOnCompletionListener(myVideoViewCompletionListener);
+        vvRandomView.setOnPreparedListener(MyVideoViewPreparedListener);
+        vvRandomView.setOnErrorListener(myVideoViewErrorListener);*/
+
+        vvRandomView.requestFocus();
+        vvRandomView.start();
+        startTimer();
 
     }
 
     private void initView() {
         vvRandomView = findViewById(R.id.vvRandomView);
-        btnLarge = findViewById(R.id.btnLarge);
-        btnSmall = findViewById(R.id.btnSmall);
 
-        btnSmall.setOnClickListener(this);
-        btnLarge.setOnClickListener(this);
-    }
+        tvStatus = findViewById(R.id.tvStatus);
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnSmall:
+        DisplayMetrics dm;
+        dm = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        height = dm.heightPixels;
+        width = dm.widthPixels;
 
-                break;
-
-            case R.id.btnLarge:
-
-                break;
-        }
+        vvRandomView.setMinimumHeight(height);
+        vvRandomView.setMinimumWidth(width);
     }
 }
